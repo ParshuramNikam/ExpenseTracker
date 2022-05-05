cf89da3ae97631355c32127888a5f6ea42c9c032
deeplahane <deeplahane15@gmail.com>
2022-05-06 02:43:11 +0530

    minor changes

--- src/Pages/PersonalExpenses.jsx
+++ src/Pages/PersonalExpenses.jsx
@@ -19,30 +19,33 @@ const PersonalExpenses = () => {
 
     return (
         <>
-            <div>
-                <h1 className='text-2xl md:text-3xl text-red-500 m-2 mb-5 font-semibold'>Personal Expenses : </h1>
+            <div className='bg-gray-100'>
+                <h1 className='text-2xl md:text-3xl text-black  pt-3 ml-3 mb-0 font-bold'>Personal Expenses</h1>
                 <div className='m-2 mb-5 md:flex justify-between'>
                     <div className='flex md:w-max items-center gap-1.5 my-4'>
                         {/* <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
                         alt='Profile Pic'
                         className='w-10 h-10 rounded-full'
                     /> */}
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
-                        </svg>
-                        <h3 className='whitespace-nowrap font-semibold text-xl'>Jhon Doe</h3>
+                        </svg> */}
+                        <p className='text-black font-normal text-xl ml-2'>Welcome back,</p>
+                        <h3 className='whitespace-nowrap text-red-500 font-semibold  text-xl'>Jhon Doe</h3>
                     </div>
 
                     {
                         !isOpen &&
-                        <button className='shadow-lg md:w-auto mx-auto md:mx-0 flex justify-center items-center gap-1 bg-red-500 hover:bg-red-600 transition duration-50 delay-100 hover:delay-100" text-white px-8 py-4 md:py-2 rounded-lg'
+                        <div className='mt-2 '>
+                        <button className='shadow-lg md:w-auto mx-auto md:mx-0 flex justify-center items-center gap-1 bg-red-500 hover:bg-gray-600 transition duration-50 delay-100 hover:delay-100" text-white px-4 py-4 md:py-2 rounded-lg'
                             onClick={onOpenModal} type="button"
                         >
-                            Add New Expense
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2 " viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
                             </svg>
+                            <p className='text-lg '>Add New Expense</p>
                         </button>
+                        </div>
                     }
 
                 </div>
@@ -90,15 +93,15 @@ const PersonalExpenses = () => {
                     </form>
                 </div>
 
-                <div className='w-max mx-auto mb-5 flex items-center gap-3 bg-gray-200 m-2 rounded px-6 sm:px-10 p-2.5 border-gray-400'>
+                <div className='border-2 w-max mx-auto mb-5 flex items-center gap-3 bg-gray-50 m-2 rounded px-6 sm:px-6 p-2.5 border-black'>
                     <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-900 h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-900 h-12 w-12 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
                     </div>
-                    <div>
-                        <div className='font-semibold text-gray-800'>Your Expense Today</div>
-                        <div className='text-xl font-semibold'>20,000 &#8377;</div>
+                    <div className='flex flex-col'>
+                        <div className='font-medium text-xl text-gray-600'>Your Expense Today</div>
+                        <div className='flex self-center text-2xl font-semibold'>&#8377; 20,000</div>
                     </div>
                     {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia tenetur nemo voluptates facere amet non laboriosam accusantium labore nisi voluptas. */}
                 </div>
--- src/components/PersonalExpenseCard.jsx
+++ src/components/PersonalExpenseCard.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const PersonalExpenseCard = () => {
-    return (
+  return (
+    <div>
+      <div className="flex border-t-4 border-red-500 md:flex justify-between gap-3 m-2 rounded-md shadow-md bg-white text-gray-700 px-4 py-3">
         <div>
-            <div className='md:flex justify-between gap-3 m-2 rounded-md shadow-md bg-red-500 text-white px-4 py-3'>
-                <div>
-                    <div className=''>Lorem ipsum dolor sit amet! Lorem ipsum dolor sit amet consectetur adipi elit. Earum voluptatem porro perferendis, corporis obcaecati at sequi sapiente nesciunt mollitia quae.</div>
-                    <div className='mt-4 text-sm'> <span className='whitespace-nowrap'>02 May 2022</span> | <span className='whitespace-nowrap'>04:40 PM</span></div>
-                </div>
-                <div className='mt-3 md:mt-0 text-2xl whitespace-nowrap text-right my-auto'>5000 &#8377;</div>
-            </div>
+          <h1 className="w-min p-2 text-red-500  bg-red-100 rounded-lg text-base font-semibold">
+            Management
+          </h1>
+          <div className="font-semibold text-black">
+            Lorem ipsum dolor sit amet! Lorem ipsum dolor sit amet consectetur
+            adipi elit. Earum voluptatem porro perferendis, corporis obcaecati
+            at sequi sapiente nesciunt mollitia quae.
+          </div>
+          <div className="mt-4 text-sm font-normal text-gray-700">
+            {" "}
+            <span className="whitespace-nowrap">02 May 2022</span> |{" "}
+            <span className="whitespace-nowrap">04:40 PM</span>
+          </div>
         </div>
-    )
-}
+          <div className=" mt-2 mr-2 text-gray-800 font-medium md:mt-0 text-3xl whitespace-nowrap text-right my-auto">
+            &#8377; 5000
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PersonalExpenseCard
+export default PersonalExpenseCard;
